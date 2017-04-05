c8336a8b9fe069f886c68e3edf4efdbedc32ef4f
alexeiffert <aeiffert@ufl.edu>
2017-04-05 19:26:47 -0400

    hi

--- AI.h
+++ AI.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef AI
+#define AI
+
+#include <string>
+
+class AI {
+	std::string difficulty;
+	int nextX;
+	int nextY;
+
+	AI(std::string difficulty = "easy");
+
+	void arrangeShip();
+	void decideAttack(int[] * board);
+	int getX
+};
+
+#endif
