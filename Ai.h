7f1cd11114d92650378873bc59391312a03c0463
alexeiffert <aeiffert@ufl.edu>
2017-04-05 21:32:57 -0400

    Work

--- Ai.h
+++ Ai.h
@@ -6,14 +6,28 @@
 class Ai {
 private:
 	std::string difficulty;
+
+	Ship carrier;
+	Ship battleship;
+	Ship cruiser;
+	Ship submarine;
+	Ship destroyer;
+
 	int nextX;
 	int nextY;
 
+	int board [10][10];
 public:
+
+	//Constructs an AI object with default difficulty "easy"
 	Ai(std::string difficulty = "easy");
 
+	//Arranges ships at the start of the game only
 	void arrangeShip();
-	void decideAttack(int[] board);
+
+	//Uses appropriate algorithm to pick attack square. Returns pointer to int[] with values (x,y)
+	int* decideAttack(int board[10][10]);
+
 	int getX();
 };
 
