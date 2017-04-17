b86cee230168d49f510ae0bef804c5da70d1c54a
harrison-hope <hhope000@gmail.com>
2017-04-16 23:41:59 -0400

    Add files via upload

---
+++ Board.cpp
@@ -0,0 +1,189 @@
+//#include "Board.h"
+#ifndef SHIP_H
+#define SHIP_H
+#include "Ship.cpp"
+#endif // SHIP_H
+#include <iomanip>
+#include <iostream>
+class Board
+{
+public:
+    /*Board();
+    bool placeShip(int x, int y, int dir, int length);
+    void fire(int x, int y, Board enemy);
+    bool checkShip(int shipNum);
+    void printBoard();*/
+
+    Ship* shipList[5];
+    int shipGrid[10][10];
+    char shipGridPrint[10][10];
+    char shotsFired[10][10];
+    int shipCount;
+    int boardLength;
+
+    Board()
+    {
+        for(int i = 0; i<10; i++)
+        {
+            for(int j = 0; j<10; j++)
+            {
+                shipGrid[i][j] = 0;
+                shipGridPrint[i][j] = '-';
+                shotsFired[i][j] = '-';
+            }
+        }
+        shipCount = 0;
+        boardLength = 10;
+    }
+
+    bool placeShip(int x, int y, int dir, int length)
+    {
+        shipList[shipCount] = new Ship(x,y,length,dir,false);
+        if(dir == 0)
+        {
+            for(int i = 0; i < length; i++)
+            {
+                if(shipGrid[x][y-i] != 0)
+                {
+                    return false;
+                }
+            }
+            for(int i = 0; i < length; i++)
+            {
+                shipGrid[x][y-i] = 1;
+                shipGridPrint[x][y-i] = shipCount+1+'0';
+            }
+        }
+        else if(dir == 1)
+        {
+            for(int i = 0; i < length; i++)
+            {
+                if(shipGrid[x+i][y] != 0)
+                {
+                    return false;
+                }
+            }
+            for(int i = 0; i < length; i++)
+            {
+                shipGrid[x+i][y] = 1;
+                shipGridPrint[x+i][y] = shipCount+1+'0';
+            }
+        }
+        else if(dir == 2)
+        {
+            for(int i = 0; i < length; i++)
+            {
+                if(shipGrid[x][y+i] != 0)
+                {
+                    return false;
+                }
+            }
+            for(int i = 0; i < length; i++)
+            {
+                shipGrid[x][y+i] = 1;
+                shipGridPrint[x][y+i] = shipCount+1+'0';
+            }
+        }
+        else
+        {
+            for(int i = 0; i < length; i++)
+            {
+                if(shipGrid[x-i][y] != 0)
+                {
+                    return false;
+                }
+            }
+            for(int i = 0; i < length; i++)
+            {
+                shipGrid[x-i][y] = 1;
+                shipGridPrint[x-i][y] = shipCount+1+'0';
+            }
+        }
+        shipCount++;
+        return true;
+    }
+
+    bool checkHit(int x, int y)
+    {
+        if(shipGrid[x][y] == 0 || shipGrid[x][y] == 2)
+        {
+            shipGrid[x][y] = 2;
+            shipGridPrint[x][y] = 'o';
+            return false;
+        }
+        else
+        {
+            shipGrid[x][y] = 3;
+            shipGridPrint[x][y] = 'x';
+            return true;
+        }
+
+    }
+
+    void fire(int x, int y, Board* enemy)
+    {
+        if(enemy->checkHit(x,y))
+        {
+            shotsFired[x][y] = 'x';
+        }
+        else
+        {
+            shotsFired[x][y] = 'o';
+        }
+    };
+
+    bool checkShip(int shipNum)
+    {
+        for(int i = 0; i < boardLength; i++)
+        {
+            for(int j = 0; j < boardLength; j++)
+            {
+                if(shipGridPrint[i][j] == shipNum+'0')
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    void printBoard()
+    {
+        for(int i = 0; i < 2*boardLength; i++)
+        {
+            cout << '-';
+        }
+        cout << endl;
+        for(int i = 0; i < boardLength; i++)
+        {
+            for(int j = 0; j < boardLength; j++)
+            {
+                cout << shotsFired[j][i] << " ";
+            }
+            cout << endl;
+        }
+        for(int i = 0; i < 2*boardLength; i++)
+        {
+            cout << '-';
+        }
+        cout << endl;
+        for(int i = 0; i < boardLength; i++)
+        {
+            for(int j = 0; j < boardLength; j++)
+            {
+                cout << shipGridPrint[j][i] << " ";
+            }
+            cout << endl;
+        }
+        for(int i = 0; i < 2*boardLength; i++)
+        {
+            cout << '-';
+        }
+        cout << endl;
+    }
+};
+
+
+
+
+
---
+++ Game.cpp
@@ -0,0 +1,204 @@
+#include <iostream>
+#include <string>
+//#include "Game.h"
+#ifndef BOARD_H
+#define BOARD_H
+#include "Board.cpp"
+#endif
+
+//#include "Ai.cpp"
+#ifndef SHIP_H
+#define SHIP_H
+#include "Ship.cpp"
+#endif // SHIP_H
+
+#include <string>
+#include <windows.h>
+
+const int player = 1;
+const int opponent = 2;
+
+
+void type_text(const std::string& text)
+{
+	// loop through each character in the text
+	for (std::size_t i = 0; i < text.size(); ++i)
+	{
+		// output one character
+		// flush to make sure the output is not delayed
+		std::cout << text[i] << std::flush;
+
+		Sleep(40); // use Sleep on windows
+	}
+}
+
+/*nt fire(std::string playerName, Board &opponentBoard)
+{
+	std::cout << "Commander " << playerName << ", standing by to fire" << std::endl
+		<< "command format is [A-F][01-10]:" << std::endl;
+	std::string target;
+	std::cin >> target;
+	int result = opponentBoard.markShot(target);
+	printBoard(opponentBoard, opponent);
+
+	return result;
+}
+
+std::string checkWinner(int shipsDestroyed, int maxShips, std::string playerName)
+{
+	std::string winner = " ";
+	if (maxShips <= shipsDestroyed)
+    {
+		winner = playerName;
+	}
+	else
+	{
+        winner = "Enemy";
+	}
+	return winner;
+}*/
+
+bool gameOver(Board* playerBoard, Board* aiBoard)
+{
+    int pShips = 0;
+    int aShips = 0;
+    for(int i = 0; i < 5; i++)
+    {
+        if(playerBoard->checkShip(i))
+        {
+            pShips++;
+        }
+        if(aiBoard->checkShip(i))
+        {
+            aShips++;
+        }
+    }
+    if (aShips >= 5)
+    {
+        type_text("Congratulations! You win!");
+        exit(0);
+		return false;
+	}
+	if (pShips >= 5)
+    {
+        type_text("You've been defeated!");
+        exit(0);
+		return false;
+	}
+	else
+    {
+        return true;
+    }
+}
+
+void placeShips(Board* board)
+{
+    std::string temp[5] = {"Destroyer", "Submarine", "Cruiser", "Carrier", "Battleship"};
+    int sizes[5] = {2,3,3,4,5};
+    int x;
+    int y;
+    int dir;
+    for(int i = 0; i < 5; i++)
+    {
+        type_text("Deploying "+temp[i]+"\n");
+        type_text("Enter X Coordinate: ");
+        while(1)
+        {
+            cin >> x;
+            if(x>10||x<1)
+            {
+                type_text("Deploy the ship within the BattleField (1-10): ");
+            }
+            else
+            {
+                break;
+            }
+        }
+        type_text("Enter Y Coordinate: ");
+        while(1)
+        {
+            cin >> y;
+            if(y>10||y<1)
+            {
+                type_text("Deploy the ship within the BattleField (1-10): ");
+            }
+            else
+            {
+                break;
+            }
+        }
+        type_text("Enter the direction the ship will face (0 = North, 1 = East, 2 = South, 3 = West): ");
+        while(1)
+        {
+            cin >> dir;
+            if(dir<0||dir>3)
+            {
+                type_text("This is not a valid direction! We're running out of time (0 = North, 1 = East, 2 = South, 3 = West): ");
+            }
+            else
+            {
+                break;
+            }
+        }
+        if(!board->placeShip(x-1,y-1,dir,sizes[i]))
+        {
+            type_text("There's no room to deploy the ship there! Try again!\n");
+            i--;
+        }
+    }
+
+}
+
+int main()
+{
+	Board* playerBoard = new Board();
+	Board* opponentBoard = new Board();
+	//Ai enemy;
+
+
+	std::cout << "   ___  ___ ______________   __________ _________ " << std::endl;
+	std::cout << "  / _ )/ _ /_  __/_  __/ /  / __/ __/ // /  _/ _ \\" << std::endl;
+	std::cout << " / _  / __ |/ /   / / / /__/ _/_\\ \\/ _  // // ___/" << std::endl;
+	std::cout << "/____/_/ |_/_/   /_/ /____/___/___/_//_/___/_/    " << std::endl;
+
+
+	type_text("Welcome to BATTLESHIP!\nPlease enter your name\nName: ");
+
+	std::string playerName;
+	std::cin >> playerName;
+
+
+	type_text("Okay " + playerName + " the enemies fleet is quickly approaching.\n");
+	type_text("Get ready to place your ships.\n");
+	type_text("You are at the command of 5 ships:\n");
+	type_text("Destroyer - Size 2\n");
+	type_text("Submarine - Size 3\n");
+	type_text("Cruiser - Size 3\n");
+	type_text("Carrier - Size 4\n");
+	type_text("Battleship - Size 5\n");
+
+    placeShips(playerBoard);
+
+    //PLACE AI SHIPS
+
+	type_text("Displaying the battlefield...\n");
+
+	playerBoard->printBoard();
+
+	int xval;
+    int yval;
+	while (!gameOver(playerBoard, opponentBoard))
+    {
+		type_text("Prepare to fire!\nInput the x-value of the attack (1-10): ");
+		std::cin>>xval;
+		type_text("Input the y-value of the attack (1-10): ");
+		std::cin>>yval;
+		std::cout << "Firing at coordinates " << xval << ", " << yval << "." << std::endl;
+		playerBoard->fire(xval-1, yval-1, opponentBoard);
+		type_text("Enemy fire detected!\n");
+		//AI ATTACK STUFF
+		std::cout << "----" << "PLAYER BOARD" << "----" << std::endl;
+		playerBoard->printBoard();
+	};
+	return 0;
+}
